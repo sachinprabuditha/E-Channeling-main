5aa120aed0ac0e2f5a45450ea34945e9952ab4fd
Sachin Prabuditha <125368889+sachinprabuditha@users.noreply.github.com>
2024-03-02 08:27:07 +0530

    Update UpdateDoctorServlet.java

--- src/main/java/com/channeling/registration/UpdateDoctorServlet.java
+++ src/main/java/com/channeling/registration/UpdateDoctorServlet.java
@@ -26,7 +26,6 @@ public class UpdateDoctorServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		
 		String id = request.getParameter("id");
 		String name = request.getParameter("name");
